2ff777a72e829894f30505cec3b0353e4230c04c
Jacka <jsturtev@terpmail.umd.edu>
2016-11-06 00:57:59 -0400

    Most Front end done, work done on card view

--- app/src/main/java/com/example/jacka/bizzconnect/CardViewer.java
+++ app/src/main/java/com/example/jacka/bizzconnect/CardViewer.java
@@ -1,5 +1,6 @@
 package com.example.jacka.bizzconnect;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -21,13 +23,10 @@ public class CardViewer extends AppCompatActivity{
     private SearchView mSearchbar;
     private ListView mListView;
     private Uri test;
-    private String name= "Justin";
-    private String mobile= "Justin";
-//    private String home= "Justin";
-//    private String work= "Justin";
-//    private String email= "Justin";
-//    private String company= "Justin";
-//    private String industry= "Justin";
+    private String name;
+    private String mobile;
+    private String email;
+    private String company;
 
 
     @Override
@@ -36,16 +35,29 @@ public class CardViewer extends AppCompatActivity{
         setContentView(R.layout.activity_viewer);
 //        mSearchbar= (SearchView) findViewById(R.id.searchView);
         mListView=(ListView) findViewById(R.id.listView);
-        Button badgeMedium = (Button) findViewById(R.id.contact1);
+        final TextView mUserName1=(TextView)findViewById(R.id.contact_name1);
+        final TextView mUserNumber1=(TextView)findViewById(R.id.contact_number1);
+        final TextView mUserEmail1=(TextView)findViewById(R.id.contact_email1);
+        final TextView mUserCompany1=(TextView)findViewById(R.id.contact_company1);
+        Button badgeMedium = (Button) findViewById(R.id.bconnect);
         badgeMedium.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CreateNewContact newContact= new CreateNewContact();
-                newContact.createContact(name, mobile, getApplicationContext());
+                newContact.createContact(mUserName1.getText().toString(), mUserNumber1.getText().toString(),
+                        mUserEmail1.getText().toString(), mUserCompany1.getText().toString(), getApplicationContext());
                 Toast.makeText(CardViewer.this, "Contact Added", Toast.LENGTH_LONG).show();
             }
         });
-
+        Button link= (Button) findViewById(R.id.blink);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.linkedin.com/in/jack-sturtevant-1161a0128?authType=NAME_SEARCH&authToken=ObRG&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A525989538%2CauthType%3ANAME_SEARCH%2Cidx%3A2-1-2%2CtarId%3A1478408016680%2Ctas%3AJack"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
         }
 
 
--- app/src/main/java/com/example/jacka/bizzconnect/CreateNewContact.java
+++ app/src/main/java/com/example/jacka/bizzconnect/CreateNewContact.java
@@ -36,11 +36,13 @@ public class CreateNewContact {
 
     }
 
-    public void createContact(String displayName, String number,Context cntx /*App or Activity Ctx*/)
+    public void createContact(String displayName, String number, String email, String company, Context cntx /*App or Activity Ctx*/)
     {
         Context contetx 	= cntx; //Application's context or Activity's context
         String strDisplayName 	=  displayName; // Name of the Person to add
         String strNumber 	=  number; //number of the person to add with the Contact
+        String strEmail = email;
+        String strCompany= company;
 
         ArrayList<ContentProviderOperation> cntProOper = new ArrayList<ContentProviderOperation>();
         int contactIndex = cntProOper.size();//ContactSize
@@ -62,7 +64,25 @@ public class CreateNewContact {
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,contactIndex)
                 .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
                 .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, strNumber) // Number to be added
-                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build()); //Type like HOME, MOBILE etc
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
+        //Type like HOME, MOBILE etc
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.DATA, strEmail)
+                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                    .build());
+
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, strCompany)
+                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+//                    .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, JobTitle)
+//                    .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, ContactsContract.CommonDataKinds.Organization.TYPE_WORK)
+                    .build());
         try
         {
             // We will do batch operation to insert all above data
